5c4d708a8dd7cc349e60e98ae4e99c7a7bd5b1ab
Volodymyr <voldemar.v.lysenko@gmail.com>
2025-08-07 20:53:02 +0300

    fix build errors

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,20 +1,26 @@
 import {
   dehydrate,
   HydrationBoundary,
-  QueryClient,
+  QueryClient
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-export default async function NoteDetailsPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+// export default async function NoteDetailsPage({
+//   params
+// }: {
+//   params: { id: string };
+// }) {
+
+export default async function NoteDetailsPage(
+  props: { params: { id: string } }
+) {
+  const { params } = props;
+
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ["note", params.id],
-    queryFn: () => fetchNoteById(params.id),
+    queryFn: () => fetchNoteById(params.id)
   });
 
   return (
